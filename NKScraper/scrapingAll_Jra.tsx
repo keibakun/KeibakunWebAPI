86f3c5bd97b64ecc68b080ccc2fe5f30208b2bb4
funadaya13 <130941125+funadaya13@users.noreply.github.com>
2025-02-15 01:26:09 +0900

    Update scrapingAll_Jra.tsx

--- NKScraper/scrapingAll_Jra.tsx
+++ NKScraper/scrapingAll_Jra.tsx
@@ -10,8 +10,9 @@ const scrapingAll_Jra = async () => {
     // レース一覧のJSONファイルを生成
     const dp = path.join(__dirname as string, "RaceList")
     // await fs.mkdir(dp);
-    const fp = path.join(dp, "index.html")
+    const fp = path.join(dp, "index0.html")
     await fs.writeFile(fp, JSON.stringify(raceListResult, null, 2), "utf-8")
+    console.log(fp);
 
     // レース一覧が取得できた場合、レースIDをキーにして出走馬を取得
     let syutubaResult: (Syutuba)[] = [];
@@ -26,7 +27,7 @@ const scrapingAll_Jra = async () => {
                 const dirPath = path.join(__dirname as string, "Shutuba/raceListResult[i].RaceID")
                 console.log(dirPath);
                 // await fs.mkdir(dirPath, { recursive: true })
-                const filePath = path.join(dirPath, 'index0.html');
+                const filePath = path.join(dirPath, 'index.html');
                 await fs.writeFile(filePath, JSON.stringify(syutubaResult, null, 2), 'utf-8');
             }
         }
