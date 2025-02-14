9277333248fd280d5222f9ce0ebd19b2ec659cc7
funadaya13 <130941125+funadaya13@users.noreply.github.com>
2025-02-15 00:48:22 +0900

    Update scrapingAll_Jra.tsx

--- NKScraper/scrapingAll_Jra.tsx
+++ NKScraper/scrapingAll_Jra.tsx
@@ -9,6 +9,7 @@ const scrapingAll_Jra = async () => {
     const raceListResult: (NextRace)[] = await scraper.getRaceList(scraper.getRaceListURL());
     // レース一覧のJSONファイルを生成
     const dp = path.join(__dirname as string, "RaceList")
+    console.log(dp);
     await fs.mkdir(dp);
     const fp = path.join(dp, "index.html")
     fs.writeFile(fp, JSON.stringify(raceListResult, null, 2), "utf-8")
@@ -16,13 +17,15 @@ const scrapingAll_Jra = async () => {
     // レース一覧が取得できた場合、レースIDをキーにして出走馬を取得
     let syutubaResult: (Syutuba)[] = [];
     if (raceListResult !== null) {
-        const raceListResultLength = raceListResult.length;
+        // const raceListResultLength = raceListResult.length;
+        const raceListResultLength = 1;
         for (let i = 0; i < raceListResultLength; i++) {
             if (raceListResult[i].RaceID) {
                 syutubaResult = await scraper.getRaceSyutuba(raceListResult[i].RaceID as string);
 
                 // syutubaresultをファイルに保存
                 const dirPath = path.join(__dirname as string, "Shutuba/raceListResult[i].RaceID")
+                console.log(dirPath);
                 await fs.mkdir(dirPath, { recursive: true });
                 const filePath = path.join(dirPath, 'index.html');
                 fs.writeFile(filePath, JSON.stringify(syutubaResult, null, 2), 'utf-8');
